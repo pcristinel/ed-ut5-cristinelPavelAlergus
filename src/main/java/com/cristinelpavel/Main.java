76656fe967527f1674ba88e808d08cd9eb1e3447
Cristinel Pavel Alergus <cristinelpavelalergus@gmail.com>
2024-12-03 00:11:35 +0100

    Init commit

---
+++ src/main/java/com/cristinelpavel/Main.java
@@ -0,0 +1,13 @@
+package com.cristinelpavel;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+    printUt5Message();
+  }
+
+  private static void printUt5Message() {
+    System.out.println("Cristinel Pavel Alergus - UT5 Git - Entornos de desarrollo - DAM 2024");
+  }
+}
